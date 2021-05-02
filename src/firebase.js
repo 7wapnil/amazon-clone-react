071a0f6c4e4260367aea3e9e238cdefc34dd1dea
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-03 00:46:18 +0530

    Remove test files. Add Header component.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCM3H5ElOlu1efOCJVdMbvh6-ypcLHKB0c",
+  authDomain: "app-210bc.firebaseapp.com",
+  projectId: "app-210bc",
+  storageBucket: "app-210bc.appspot.com",
+  messagingSenderId: "1079355645944",
+  appId: "1:1079355645944:web:7a03bb97dffc5adc95800d"
+};
