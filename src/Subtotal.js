587982eeeaa56a58af9233d558beebf4a89baaa7
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-06 02:39:59 +0530

    Install currency-format package.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Subtotal.css";
+import CurrenctFormat from "react-currency-format";
 
 function Subtotal() {
   return (
     <div className="subtotal">
-      <h2>Subtotal goes here</h2>
+      <CurrenctFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandsSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to checkout</button>
     </div>
   );
 }
