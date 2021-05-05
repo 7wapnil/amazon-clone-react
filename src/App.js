f5fd350a38e3aab3ebec8c4ea4dfe0c354db1faf
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-06 01:38:59 +0530

    Setup router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+            <Home />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
