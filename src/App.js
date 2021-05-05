61ada4f3e924de0b8ebcf78a5c24d3ad756e202d
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-06 01:43:29 +0530

    Add checkout component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           </Route>
           <Route path="/checkout">
             <Header />
-            <h1>Checkout Page</h1>
+            <Checkout />
           </Route>
         </Switch>
       </div>
