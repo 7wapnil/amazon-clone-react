f6cf00f8fbcd208d5df5c9c95c39477858cd5e56
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-06 02:15:31 +0530

    Add Routing links for cart and home. Add Subtotal component.

---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subtotal.css";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <h2>Subtotal goes here</h2>
+    </div>
+  );
+}
+
+export default Subtotal;
