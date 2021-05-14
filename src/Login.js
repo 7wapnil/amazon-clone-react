3a49e2b553cdabaf5902bab07e46d14a3adf061a
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-15 00:54:34 +0530

    Wire up login inputs.

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = e => {
+    e.preventDefault();
+  }
+
+  const register = e => {
+    e.preventDefault();
+  }
+  
   return (
     <div className="login">
       <Link to="/">
@@ -15,16 +27,31 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >Sign In</button>
         </form>
         <p>
           By continuing, you agree to Amazon's Conditions of Use and Privacy
           Notice.
         </p>
-        <button className="login__registerButton">
+        <button
+          className="login__registerButton"
+          onClick={register}
+        >
           Create your Amazon Account
         </button>
       </div>
