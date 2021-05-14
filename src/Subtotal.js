d43753605c352e0545be89cf0b28e911e65e5454
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-14 22:06:24 +0530

    Add react context API. Dispatch actions on adding to basket. Make cart item count and cart price dynamic.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Subtotal.css";
-import CurrenctFormat from "react-currency-format";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
-      <CurrenctFormat
+      <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,7 +19,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={basket.reduce((totalPrice, item) => totalPrice + item?.price, 0)}
         displayType={"text"}
         thousandsSeparator={true}
         prefix={"$"}
