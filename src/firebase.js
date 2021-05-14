dcfd3c30dc06e04e38df5ca467d5336f803a0b31
Swapnil Sable <swapnil.sable@outlook.com>
2021-05-15 02:36:31 +0530

    Configure firebase. Implement authentication with firebase.

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCM3H5ElOlu1efOCJVdMbvh6-ypcLHKB0c",
   authDomain: "app-210bc.firebaseapp.com",
   projectId: "app-210bc",
   storageBucket: "app-210bc.appspot.com",
   messagingSenderId: "1079355645944",
-  appId: "1:1079355645944:web:7a03bb97dffc5adc95800d"
+  appId: "1:1079355645944:web:7a03bb97dffc5adc95800d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
